f6b3e2580097051d7b3b64e68f95421fb6c68ef1
Christoph Karlen <christoph.karlen@ffhs.ch>
2023-08-30 15:23:39 +0200

    Activate db seeders for flights, passengers ans so on

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\Airplane;
 use App\Models\Airport;
 use App\Models\Flight;
+use App\Models\Passenger;
+use App\Models\PassengerFlight;
 use Filament\Facades\Filament;
 use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Auth\EloquentUserProvider;
@@ -35,27 +37,81 @@ class DatabaseSeeder extends Seeder
             'password'=>Hash::make('admin@bfo.ch'),
         ]);
 
-        //$plane747 = Airplane::create([
-        //    'typ'=>'747',
-        //]);
-        //
-        //$airportZrh = Airport::create([
-        //    'short_name'=>'ZRH',
-        //    'name'=>'Zürich',
-        //]);
-        //
-        //$airportLax = Airport::create([
-        //    'short_name'=>'LAX',
-        //    'name'=>'Los Angeles',
-        //]);
-        //
-        //Flight::create([
-        //    'number'=>'255686',
-        //    'departure_date'=>now(),
-        //    'arrival_date'=>now(),
-        //    'airplane_id'=>$plane747->id,
-        //    'start_airport_id'=>$airportZrh->id,
-        //    'end_airport_id'=>$airportLax->id,
-        //]);
+        $plane747 = Airplane::create([
+            'typ'=>'747',
+        ]);
+
+        $planeA380 = Airplane::create([
+            'typ'=>'A380',
+        ]);
+
+        $airportZrh = Airport::create([
+            'short_name'=>'ZRH',
+            'name'=>'Zürich',
+        ]);
+
+        $airportLax = Airport::create([
+            'short_name'=>'LAX',
+            'name'=>'Los Angeles',
+        ]);
+
+        $airportJfk = Airport::create([
+            'short_name'=>'JFK',
+            'name'=>'New York',
+        ]);
+
+        $flight1 = Flight::create([
+            'number'=>255686,
+            'departure_date'=>now(),
+            'arrival_date'=>now(),
+            'airplane_id'=>$plane747->id,
+            'start_airport_id'=>$airportZrh->id,
+            'end_airport_id'=>$airportLax->id,
+        ]);
+
+        $flight2 = Flight::create([
+            'number'=>13586,
+            'departure_date'=>now(),
+            'arrival_date'=>now(),
+            'airplane_id'=>$planeA380->id,
+            'start_airport_id'=>$airportZrh->id,
+            'end_airport_id'=>$airportJfk->id,
+        ]);
+
+        $flight3 = Flight::create([
+            'number'=>988856,
+            'departure_date'=>now(),
+            'arrival_date'=>now(),
+            'airplane_id'=>$planeA380->id,
+            'start_airport_id'=>$airportJfk->id,
+            'end_airport_id'=>$airportZrh->id,
+        ]);
+
+        $passenger1 = Passenger::create([
+            'email' => 'test1@bfo.ch'
+        ]);
+
+        $passenger2 = Passenger::create([
+            'email' => 'test2@bfo.ch'
+        ]);
+
+        $passenger3 = Passenger::create([
+            'email' => 'test3@bfo.ch'
+        ]);
+
+        PassengerFlight::create([
+            'flight_id' => $flight1->id,
+            'passenger_id' => $passenger1->id
+        ]);
+
+        PassengerFlight::create([
+            'flight_id' => $flight2->id,
+            'passenger_id' => $passenger1->id
+        ]);
+
+        PassengerFlight::create([
+            'flight_id' => $flight2->id,
+            'passenger_id' => $passenger2->id
+        ]);
     }
 }
